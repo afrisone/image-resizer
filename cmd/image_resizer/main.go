76a5fe239e1749130fb356e3bf20e07508ca5bf8
Alicia <a.frisone54@gmail.com>
2026-01-04 20:54:07 -0700

    Import image from filesystem and resize (#2)

---
+++ cmd/image_resizer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	_ "image/jpeg"
+
+	"github.com/afrisone/image_resizer/internal/filesystem"
+	"github.com/afrisone/image_resizer/internal/image_processor"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, err := filesystem.OpenImage(filepath.Join(wd, "src_images", "pic.jpg"))
+	if err != nil {
+		fmt.Printf("Error during import: %v\n", err)
+		os.Exit(1)
+	}
+
+	image_processor.Resize(img)
+
+}
---
+++ internal/store/local.go
@@ -0,0 +1,7 @@
+package local_store
+
+import "fmt"
+
+func Store() {
+	fmt.Println("Hello from store!!")
+}
